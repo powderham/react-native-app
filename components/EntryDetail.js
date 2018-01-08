fc666bbb909f58d4d83a8e99bb7e3dd1f0083ff5
Edward Powderham <edpowderham@outlook.com>
2018-01-08 10:01:21 +0000

    using props passed in stack navigation

--- components/EntryDetail.js
+++ components/EntryDetail.js
@@ -2,6 +2,13 @@ import React, { Component } from "react";
 import { View, Text } from "react-native";
 
 class EntryDetail extends Component {
+  static navigationOption = ({ navigation }) => {
+    const { entryId } = navigation.state.params;
+
+    return {
+      title: `${month}/${day}/${year}`
+    };
+  };
   render() {
     return (
       <View>
