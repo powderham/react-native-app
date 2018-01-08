8ae11aa40ae6ffe4cbe85d87623dc0ed24064d8e
Edward Powderham <edpowderham@outlook.com>
2018-01-08 09:34:12 +0000

    adding simple navigation

--- App.js
+++ App.js
@@ -1,10 +1,45 @@
 import React from "react";
-import { StyleSheet, Text, View, Slider } from "react-native";
+import { StyleSheet, Text, View, Slider, Platform } from "react-native";
 import AddEntry from "./components/AddEntry";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
 import reducer from "./reducers";
+import History from "./components/History";
+import { TabNavigator } from "react-navigation";
+import { purple, white } from "./utils/colors";
+import { FontAwesome, Ionicons } from "@expo/vector-icons";
 
+const Tabs = TabNavigator(
+  {
+    History: {
+      screen: History,
+      navigationOptions: {
+        tabBarLabel: "History",
+        tabBarIcon: ({ tintColor }) => (
+          <Ionicons name="ios-bookmarks" size={30} color={tintColor} />
+        )
+      }
+    },
+    AddEntry: {
+      screen: AddEntry,
+      navigationOptions: {
+        tabBarLabel: "Add Entry",
+        tabBarIcon: ({ tintColor }) => (
+          <FontAwesome name="plus-square" size={30} color={tintColor} />
+        )
+      }
+    }
+  },
+  {
+    tabBarOptions: {
+      activeTintColor: Platform.OS === "ios" ? purple : white,
+      style: {
+        height: 56,
+        backgroundColor: Platform.OS === "ios" ? white : purple
+      }
+    }
+  }
+);
 export default class App extends React.Component {
   state = {
     value: 0
@@ -13,8 +48,8 @@ export default class App extends React.Component {
   render() {
     return (
       <Provider store={createStore(reducer)}>
-        <View>
-          <AddEntry />
+        <View style={{ flex: 1 }}>
+          <History />
         </View>
       </Provider>
     );
--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,12 +1,18 @@
 import React, { Component } from "react";
 import { View, Text, TouchableOpacity } from "react-native";
-import { getMetricMetaInfo, timeToString } from "../utils/helpers";
+import {
+  getMetricMetaInfo,
+  timeToString,
+  getDailyReminderValue
+} from "../utils/helpers";
 import UdaciSlider from "./UdaciSlider";
 import UdaciSteppers from "./UdaciSteppers";
 import DateHeader from "./DateHeader";
 import { Ionicons } from "@expo/vector-icons";
 import TextButton from "./TextButton";
 import { submitEntry, removeEntry } from "../utils/api";
+import { connect } from "react-redux";
+import { addEntry } from "../actions";
 
 function SubmitBtn({ onPress }) {
   return (
@@ -16,7 +22,7 @@ function SubmitBtn({ onPress }) {
   );
 }
 
-export default class AddEntry extends Component {
+class AddEntry extends Component {
   state = {
     run: 0,
     bike: 0,
@@ -56,7 +62,11 @@ export default class AddEntry extends Component {
     const key = timeToString();
     const entry = this.state;
 
-    // Update redux
+    this.props.dispatch(
+      addEntry({
+        [key]: entry
+      })
+    );
 
     this.setState({
       run: 0,
@@ -68,14 +78,18 @@ export default class AddEntry extends Component {
 
     // Navigate to home
 
-    submit({ key, entry });
+    submitEntry({ key, entry });
 
     // Clear local notification
   };
   reset = () => {
     const key = timeToString();
 
-    // Update Redux
+    this.props.dispatch(
+      addEntry({
+        [key]: getDailyReminderValue()
+      })
+    );
 
     // Route to home
 
@@ -128,3 +142,13 @@ export default class AddEntry extends Component {
     );
   }
 }
+
+function mapStateToProps(state) {
+  const key = timeToString();
+
+  return {
+    alreadyLogged: state[key] && typeof state[key].today === "undefined"
+  };
+}
+
+export default connect(mapStateToProps)(AddEntry);
